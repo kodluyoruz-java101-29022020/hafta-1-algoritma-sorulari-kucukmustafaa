db545ba991507faf1e6741b61c13d1f73b84ad02
kucukmustafaa <mustafakucukk97@gmail.com>
2020-03-05 13:56:07 +0300

    odev1haftabitti

--- src/com/company/Odev2.java
+++ src/com/company/Odev2.java
@@ -1,4 +1,5 @@
 package com.company;
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
 
 import java.util.Scanner;
 
@@ -7,6 +8,36 @@ public class Odev2 {
         System.out.println("bir secenek seciniz...: ");
         Scanner scanner=new Scanner(System.in);
         byte input=scanner.nextByte();
+        if (input < 1 || input > 4) {
+            System.out.println("lutfen dogru secenek ile tekrar deneyiniz :)");
+            System.exit(1);
+        }
+        int a,b,sonuc ;
+        System.out.print("bir sayi giriniz...: ");
+        a=scanner.nextInt();
+        System.out.print("bir sayi giriniz...: ");
+        b=scanner.nextInt();
 
+        switch (input){
+            case 1:
+                sonuc=a+b;
+                System.out.println("sonuc...: "+sonuc);
+                break;
+            case 2:
+                sonuc=a-b;
+                System.out.println("sonuc...: "+sonuc);
+                break;
+            case 3:
+                sonuc=a*b;
+                System.out.println("sonuc...: "+sonuc);
+
+                break;
+            case 4:
+                sonuc=a/b;
+                System.out.println("sonuc...: "+sonuc);
+                break;
+            default:
+                System.out.println("lutfen dogru secenek giriniz...: ");
+        }
     }
 }
